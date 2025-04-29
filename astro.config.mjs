b04bed08bb70c78c86d1a04187fe5ff381f9bdef
jcarlos5 <josecarlosvilchezvillegas@gmail.com>
2025-04-29 01:17:29 -0500

    portfolio v0.1 - Build

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jcarlos5.github.io',
+  base: 'portfolio-v0.1',
   i18n: {
       locales: ["es", "en"],
       defaultLocale: "es",
